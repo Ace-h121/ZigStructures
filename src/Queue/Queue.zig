6359a104d2e87da71100c08e8715b01489da80bb
Ace-h121 <chathaway121@gmail.com>
2025-07-27 22:28:50 -0400

    Brain fog going woo ill implement this tomorrow

--- src/Queue/Queue.zig
+++ src/Queue/Queue.zig
@@ -1,10 +1,1 @@
-const std = @import("std");
-const List = @import("../DoubleLinkedList/DoubleLinkedList.zig");
 
-pub fn FIFOQueue(comptime T: type) type {
-    return struct {
-        const L = List.makeDoublelyLinkedList(T);
-
-        list: L = L{ .head = null },
-    };
-}
